4185b69d57bf882ce6def8e2f7a96cff40b97c64
pranali1237 <95729325+pranali1237@users.noreply.github.com>
2022-03-10 07:05:48 -0800

    Add files via upload

---
+++ CollegeManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.ClgManegementSys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollegeManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollegeManagementSystemApplication.class, args);
+	}
+
+}
